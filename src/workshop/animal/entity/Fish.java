a9d7eca469e5e76fffac9b3c62efb212a0511b55
jero-06 <jeeyoung345@gmail.com>
2026-08-26 17:21:58 +0900

    Fish add

---
+++ src/workshop/animal/entity/Fish.java
@@ -0,0 +1,29 @@
+package workshop.animal.entity;
+
+public abstract class Fish extends Animal implements Pet {
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setName(String name) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void play() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void eat() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
