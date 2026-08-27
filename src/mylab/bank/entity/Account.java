be49b619477f7ce71559f047d71380173fe2f40c
jero-06 <jeeyoung345@gmail.com>
2026-08-28 01:45:55 +0900

    Account add

--- src/mylab/bank/entity/Account.java
+++ src/mylab/bank/entity/Account.java
@@ -1,32 +1,51 @@
 package mylab.bank.entity;
 
-public class Account {
-	private String accountNumber;
-	private String ownerName;
-	private double blanace;
-	
-	public Account(String, String, double) {
-		this.accountNumber = String;
-		this.ownerName = String;
-		this.blanace = double
-	}
+import mylab.bank.exception.InsufficientBalanceException;
 
+// 추상 클래스
+public abstract class Account {
+    private String accountNumber;
+    private String ownerName;
+    protected double balance;
+
+    public Account(String accountNumber, String ownerName, double balance) {
+        this.accountNumber = accountNumber;
+        this.ownerName = ownerName;
+        this.balance = balance;
+    }
+
+    //Getter
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public String getOwnerName() {
+        return ownerName;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    // 입금 
+    public void deposit(double amount) {
+        if (amount > 0) {
+            this.balance += amount;
+            System.out.println(String.format("%.1f원이 입금되었습니다. 현재 잔액: %.1f원", amount, this.balance));
+        }
+    }
+
+    // 출금 (잔액 부족 시 InsufficientBalanceException 예외 발생)
+    public void withdraw(double amount) throws InsufficientBalanceException {
+        if (amount > this.balance) {
+            throw new InsufficientBalanceException("잔액이 부족합니다.");
+        }
+        this.balance -= amount;
+        System.out.println(String.format("%.1f원이 출금되었습니다. 현재 잔액: %.1f원", amount, this.balance));
+    }
+
+    @Override
+    public String toString() {
+        return String.format("계좌번호: %s, 소유자: %s, 잔액: %.1f원", accountNumber, ownerName, balance);
+    }
 }
-
-	public String getAccountNumber() {
-		return accountNumber;
-	}
-
-	public String getOwnerName() {
-		return ownerName;
-	}
-
-	public double getBlanace() {
-		return blanace;
-	}
-
-	public void setBlanace(double blanace) {
-		this.blanace = blanace;
-	}
-	
-	public void 
