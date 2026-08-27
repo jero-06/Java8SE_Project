cfb38b205e3d3bc8c4a5731df46a74c5c2605201
jero-06 <jeeyoung345@gmail.com>
2026-08-27 09:47:27 +0900

    Student add

--- src/mylab/student/entity/Student.java
+++ src/mylab/student/entity/Student.java
@@ -5,5 +5,30 @@ public class Student {
 	private String name;
 	private String major;
 	private int grade;
+	
+	public String getStudentId() {
+		return studentId;
+	}
+	public void setStudentId(String studentId) {
+		this.studentId = studentId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getMajor() {
+		return major;
+	}
+	public void setMajor(String major) {
+		this.major = major;
+	}
+	public int getGrade() {
+		return grade;
+	}
+	public void setGrade(int grade) {
+		this.grade = grade;
+	}
 
 }
